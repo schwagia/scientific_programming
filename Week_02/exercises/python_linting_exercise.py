9544867362a63e9944b086c409a1e7dde1d86eeb
schwagia <schwagia@students.zhaw.ch>
2025-03-03 15:07:58 +0000

    Challenge Week 2 - Lösung GR

--- Week_02/exercises/python_linting_exercise.py
+++ Week_02/exercises/python_linting_exercise.py
@@ -1,14 +1,13 @@
 # Import libraries
 import pandas as pd
-import numpy as np
 
 # Initialize data of lists
-data =                pd.DataFrame({'Name': ['tom', 'nick', 'krish', 'jack']          ,
+data = pd.DataFrame({'Name': ['tom', 'nick', 'krish', 'jack'],
                      'Age': [20, 21, 19, 18]})
 
 # Create DataFrame
-df = pd.DataFrame(                  data)       
-print(       type(df)               )
+df = pd.DataFrame(data)
+print(type(df))
 
 # Print the output
 print(df.head())
